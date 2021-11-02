86e865fb46e90f203c4b4b1a4862a2cec27928d1
Your Name <you@example.com>
2021-11-02 22:53:31 +0100

    stuff

--- sfml-formation-1/sfml-formation-1/sfml-example-1.cpp
+++ sfml-formation-1/sfml-formation-1/sfml-example-1.cpp
@@ -3,12 +3,16 @@ using namespace sf;
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
     Texture texture;
-    texture.loadFromFile("ghost.png");
+    //texture.loadFromFile("ghost.png");
+    texture.loadFromFile("foresttiles2-t.png");
     Sprite sprite;
     sprite.setTexture(texture);
+    sprite.setScale(4, 4);
+    sprite.setTextureRect(IntRect(Vector2i(20,24),Vector2i(30,33)));
+    // https://imgur.com/a/vVmZnXt
 
     while (window.isOpen())
     {
@@ -20,7 +24,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        //window.draw(shape);
         window.draw(sprite);
         window.display();
     }
