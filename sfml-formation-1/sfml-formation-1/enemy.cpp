a4c3a4116463f9dfc8770db518b5ea0415cd3a7b
Morgan Bisson <morganinho1997@outlook.fr>
2021-11-11 17:39:35 +0100

    mouvement ennemi

--- sfml-formation-1/sfml-formation-1/enemy.cpp
+++ sfml-formation-1/sfml-formation-1/enemy.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include "enemy.h"
 
 using namespace sf;
@@ -9,13 +10,54 @@ void Enemy::loadEnemy(Texture& enemyTexture)
     _enemySprite.setTexture(enemyTexture);
     _enemySprite.setTextureRect(IntRect(96, 64, 16, 16));
     _enemySprite.scale(3, 3);
-    _enemySprite.setPosition(200, 100);
+    _enemySprite.setPosition(startPos.x, startPos.y);
 }
 
-
-void Enemy::animate()
+void Enemy::moveEnemy()
 {
-	
+    
+    Vector2f enemyPos = _enemySprite.getPosition();
+    
+    // Vector normalization
+    /*float norme = std::sqrt(enemyPos.x * endPos.x + enemyPos.y * endPos.y);
+    Vector2f enemyDirection = enemyPos / norme;*/
+
+
+    if ( enemyPos.x >= endPos.x && enemyPos.y >= endPos.y)
+    {
+        _enemySprite.move((endPos.x - enemyPos.x) * _velocity,  (endPos.y - enemyPos.y)* _velocity );
+        this->animate(80);
+    }
+    else if (enemyPos.x <= endPos.x && enemyPos.y <= endPos.y)
+    {
+        _enemySprite.move((endPos.x - enemyPos.x) * _velocity, (endPos.y - enemyPos.y) * _velocity);
+        this->animate(96);
+    }
+}
+
+void Enemy::animate(int spritePosY)
+{
+    IntRect enemyRect(_enemySprite.getTextureRect());
+
+    enemyRect.top = spritePosY;
+
+    float elapsedTimeInSeconds = clock.getElapsedTime().asSeconds();
+
+    if (elapsedTimeInSeconds > 0.1f)
+    {
+        if (enemyRect.left == 128)
+        {
+            enemyRect.left = 96;
+        }
+        else
+        {
+            enemyRect.left += 16;
+        }
+
+        _enemySprite.setTextureRect(enemyRect);
+        clock.restart();
+
+    }
 
 }
 
--- sfml-formation-1/sfml-formation-1/enemy.h
+++ sfml-formation-1/sfml-formation-1/enemy.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 
+
 class Enemy
 {
 public:
@@ -8,14 +9,24 @@ public:
 	~Enemy();*/
 
 	void loadEnemy(sf::Texture& t);
-	void animate();
-
+	void animate(int spritePosY);
+	void moveEnemy();
 	void update(float delta);
 	void draw(sf::RenderWindow& window);
 
 	sf::Sprite getSprite() { return _enemySprite; };
+
 protected:
-	int _velocity = 0;
+
+	float _velocity = 0.002;
+	int pauseTime = 2;
 	sf::Sprite _enemySprite;
+
+	sf::Vertex enemyVertex;
+
+	sf::Clock clock;
+
+	sf::Vector2f startPos{ 200, 100 };
+	sf::Vector2f endPos{300 ,200};
 };
 
