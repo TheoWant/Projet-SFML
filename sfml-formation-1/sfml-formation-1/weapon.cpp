2440358bf3f9f1b3c46e51b7f466a7e0bd4b594a
Morgan Bisson <morganinho1997@outlook.fr>
2021-11-12 17:33:25 +0100

    Création class weapon

--- sfml-formation-1/sfml-formation-1/weapon.cpp
+++ sfml-formation-1/sfml-formation-1/weapon.cpp
@@ -1,1 +1,18 @@
+#include <SFML/Graphics.hpp>
 #include "weapon.h"
+
+using namespace sf; 
+
+
+void Weapon::drawWeapon(RenderWindow& window)
+{
+    window.draw(_weaponSprite);
+}
+
+void Weapon::loadWeapon(Texture& weaponTexture)
+{
+    weaponTexture.loadFromFile("sword_normal.png");
+    _weaponSprite.setTexture(weaponTexture);
+    _weaponSprite.scale(1, 1);
+    _weaponSprite.setPosition(200, 300);
+}
--- sfml-formation-1/sfml-formation-1/weapon.h
+++ sfml-formation-1/sfml-formation-1/weapon.h
@@ -1,5 +1,13 @@
 #pragma once
-class weapon
+class Weapon
 {
+public:
+	void loadWeapon(sf::Texture& texture);
+	void drawWeapon(sf::RenderWindow& window);
+
+protected:
+	int _damage = 10;
+	sf::Sprite _weaponSprite;
+
 };
 
