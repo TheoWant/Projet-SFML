ca207c9b1a0567c3003e8649bd027839f8c3b908
mbisson <mbisson@GCFRLY147>
2021-11-16 13:04:57 +0100

    weapon

--- sfml-formation-1/sfml-formation-1/weapon.cpp
+++ sfml-formation-1/sfml-formation-1/weapon.cpp
@@ -30,20 +30,56 @@ sf::Vector2f Weapon::getWeaponPosition()
 {
     return _weaponSprite.getPosition();
 }
+bool Weapon::attack()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    {
+        
+    }
+}
 
-void Weapon::Animate(RenderWindow& window, Texture& weaponTexture, int playerPosX, int playerPosY, int LastPosY) {
-    if (hasWeapon == true){
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+
+void Weapon::Animate(RenderWindow& window, Texture& weaponTexture, int playerPosX, int playerPosY, int LastPosY) 
+{
+    float deltaTime = timer.restart().asSeconds();
+
+    attackTimer += deltaTime;
+
+    _weaponSprite.setOrigin(0, 0);
+
+    if (hasWeapon == true) {
+        if (LastPosY == 0) {
+            _weaponSprite.setPosition(playerPosX + 22, playerPosY + 12);
+            angleStart = 340;
+            _weaponSprite.setRotation(angleStart);
+            if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Space))) {
+
+            }
+        }
+        if (LastPosY == 32) {
+            _weaponSprite.setPosition(playerPosX + 25, playerPosY + 12);
+            _weaponSprite.setRotation(0);
+        }
+        if (LastPosY == 16) {
+            _weaponSprite.setPosition(playerPosX - 10, playerPosY + 40);
+            _weaponSprite.setRotation(270);
+        }
+        if (LastPosY == 48) {
+            _weaponSprite.setPosition(playerPosX + 15, playerPosY + 12);
+            _weaponSprite.setRotation(320);
+        }
+        
+    }
+
+    if ()
+        if (_weaponSprite.getRotation() >= 340 && _weaponSprite.getRotation() <= 345)
         {
-            if(LastPosY == 0 or LastPosY == 32 or LastPosY == 48){
-                _weaponSprite.setPosition(playerPosX + 25 , playerPosY + 12);
-                _weaponSprite.setRotation(0);
-            }
-            else if (LastPosY == 16) {
-                _weaponSprite.setPosition(playerPosX - 10, playerPosY + 40);
-                _weaponSprite.setRotation(270);
-            }
-            window.draw(_weaponSprite);
+            if (angleEnd >= angleStart)
+
+            _weaponSprite.setRotation(angleStart);
+            _weaponSprite.setOrigin(20, 30);
+            angleStart += 5;
         }
     }
+    window.draw(_weaponSprite);
 }
--- sfml-formation-1/sfml-formation-1/weapon.h
+++ sfml-formation-1/sfml-formation-1/weapon.h
@@ -5,6 +5,9 @@ public:
 	void loadWeapon(sf::Texture& texture);
 	void drawWeapon(sf::RenderWindow& window);
 	void pickUp();
+
+	bool attack();
+
 	void Animate(sf::RenderWindow& window, sf::Texture& weaponTexture, int playerPosX, int playerPosY,int lastPosY);
 	sf::Vector2f getWeaponPosition();
 
@@ -12,7 +15,17 @@ public:
 
 protected:
 	int _damage = 10;
+
+	int _attackSpeed = 10;
+
 	sf::Sprite _weaponSprite;
 
+	sf::Clock timer;
+	sf::Time deltaTime;
+
+	float attackTimer = 0;
+
+	float angleStart = 0;
+	float angleEnd = 90;
 };
 
